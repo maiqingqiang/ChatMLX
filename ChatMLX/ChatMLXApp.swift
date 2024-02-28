bbbfea288752ceb393c9ebc4d6a2116e4aa10f63
John Mai <maiqingqiang@foxmail.com>
2024-02-28 21:29:26 +0800

    Initial Commit

---
+++ ChatMLX/ChatMLXApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChatMLXApp.swift
+//  ChatMLX
+//
+//  Created by John Mai on 2024/2/28.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChatMLXApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ChatMLXUITests/ChatMLXUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChatMLXUITestsLaunchTests.swift
+//  ChatMLXUITests
+//
+//  Created by John Mai on 2024/2/28.
+//
+
+import XCTest
+
+final class ChatMLXUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
