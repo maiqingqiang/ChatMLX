4f01837863c7f399b3600f5d95b38df3d4f58375
John Mai <maiqingqiang@foxmail.com>
2024-09-01 14:19:09 +0800

    fix window

--- ChatMLX/Components/UltramanMinimalistWindowModifier.swift
+++ ChatMLX/Components/UltramanMinimalistWindowModifier.swift
@@ -5,20 +5,22 @@
 //  Created by John Mai on 2024/8/10.
 //
 
+import AppKit
+import Defaults
 import SwiftUI
 import SwiftUIIntrospect
-import Defaults
 
 struct UltramanMinimalistWindowModifier: ViewModifier {
-    private var fullScreenPresentationWindowDelegate = FullScreenPresentationWindowDelegate()
     @Default(.backgroundBlurRadius) var blurRadius
     @Default(.backgroundColor) var backgroundColor
-    
+    @State private var isFullScreen = false
+
     func body(content: Content) -> some View {
         content
             .ignoresSafeArea()
             .introspect(.window, on: .macOS(.v14, .v15)) { window in
-                window.setBackgroundBlur(radius: Int(blurRadius),color: NSColor(backgroundColor))
+                window.setBackgroundBlur(
+                    radius: Int(blurRadius), color: NSColor(backgroundColor))
                 window.toolbarStyle = .unified
                 window.titlebarAppearsTransparent = true
                 window.titleVisibility = .hidden
@@ -26,8 +28,37 @@ struct UltramanMinimalistWindowModifier: ViewModifier {
                 let toolbar = NSToolbar()
                 toolbar.showsBaselineSeparator = false
                 window.toolbar = toolbar
-                window.delegate = fullScreenPresentationWindowDelegate
+
+                NotificationCenter.default.addObserver(
+                    forName: NSWindow.didEnterFullScreenNotification,
+                    object: window, queue: .main
+                ) { _ in
+                    self.isFullScreen = true
+                    self.updateFullScreenSettings(for: window)
+                }
+
+                NotificationCenter.default.addObserver(
+                    forName: NSWindow.didExitFullScreenNotification,
+                    object: window, queue: .main
+                ) { _ in
+                    self.isFullScreen = false
+                    self.updateFullScreenSettings(for: window)
+                }
             }
+    }
+
+    private func updateFullScreenSettings(for window: NSWindow) {
+        if isFullScreen {
+            window.collectionBehavior.insert(.fullScreenPrimary)
+            window.toolbar?.isVisible = false
+            NSApp.presentationOptions = [
+                .autoHideToolbar, .autoHideMenuBar, .fullScreen,
+            ]
+        } else {
+            window.collectionBehavior.remove(.fullScreenPrimary)
+            window.toolbar?.isVisible = true
+            NSApp.presentationOptions = []
+        }
     }
 }
 
